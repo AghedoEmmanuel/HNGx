d2257f9bb00ff226792c71447cddae5297b60389
AghedoEmmanuel <emmaodianose@gmail.com>
2023-09-14 02:27:22 +0100

    completed

--- stage_two/src/components/Card/Card.jsx
+++ stage_two/src/components/Card/Card.jsx
@@ -8,41 +8,41 @@ function Card({ movie, selectMovie }) {
   const formatDateToUTC = (dateString) => {
     const options = { timeZone: 'UTC', year: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
-};
+  };
 
-    return (
-        <Link to={`/movie/${movie.id}`} >
-        <div className="space-y-2" onClick={() => selectMovie(movie)}>
-          <div className="w-[100%]" data-testid="movie-poster">
-            {movie.poster_path ? (
-              <div className='relative'>
+  return (
+    <Link to={`/movie/${movie.id}`} >
+      <div className="space-y-2" onClick={() => selectMovie(movie)}>
+        <div className="w-[100%]" data-testid="movie-poster">
+          {movie.poster_path ? (
+            <div className='relative'>
               <img
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 className="w-full "
                 alt={movie.title}
               />
-             <div className='absolute top-2 right-3 hover:bg-rose-500'>
-             <img src={fav} alt='fav-icon' />
-             </div>
+              <div className='absolute top-2 right-3 hover:bg-rose-500'>
+                <img src={fav} alt='fav-icon' />
               </div>
-            ) : (
-              <div className="min-h-[18rem] flex justify-center items-center border-solid border-2 border-black">
-                No Image Found
-              </div>
-            )}
-          </div>
-          <p data-testid="movie-release-date">{formatDateToUTC(movie.release_date)}</p>
-          <h3 className="font-bold" data-testid="movie-title">
-            {movie.title}
-          </h3>
-          <p className="flex items-center">
-            <img src={imdb} className="pr-2" alt="IMDb" />
-            {movie.vote_average}/10
-          </p>
-          </div>
+            </div>
+          ) : (
+            <div className="min-h-[18rem] flex justify-center items-center border-solid border-2 border-black">
+              No Image Found
+            </div>
+          )}
+        </div>
+        <p data-testid="movie-release-date">{formatDateToUTC(movie.release_date)}</p>
+        <h3 className="font-bold" data-testid="movie-title">
+          {movie.title}
+        </h3>
+        <p className="flex items-center">
+          <img src={imdb} className="pr-2" alt="IMDb" />
+          {parseFloat(movie.vote_average).toFixed(1)}/10
+        </p>
+      </div>
     </Link>
 
-    )
+  )
 }
 
 export default Card
